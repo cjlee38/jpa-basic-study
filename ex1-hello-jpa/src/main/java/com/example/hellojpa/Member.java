3d0e50046c00ea8e746e4fc5e927e021fd7c264c
cjlee38 <gptpem38@gmail.com>
2021-07-11 02:10:57 +0900

    ADD : 엔티티 매핑

--- ex1-hello-jpa/src/main/java/com/example/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/com/example/hellojpa/Member.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.Date;
 
 //create table member (
 //        id bigint not null,
@@ -17,11 +18,49 @@ import javax.persistence.*;
 @Setter
 @ToString
 //@Table(name = "user") // 만약 db table 이름이 user라면 이렇게 하면 됨.
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+) // 시퀀스를 별도로 적용하고 싶을 경우.
 public class Member {
-    
+
+    /*
     @Id @GeneratedValue
     private Long    id;
 
 //    @Column(name = "username") // 만약 db column 이름이 username이라면 이렇게 하면 됨.
+    // JPA 실행에는 영향을 주지 않고, DDL 생성에만 영향을 준다.
+    // unique 속성, length 속성 등을 줄 수 있음.
+    // unique -> PK처럼 unique한 값을 가져야 함.
+    // length -> 길이가 10자 제한이어야 함.
     private String  name;
+     */
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR"
+    )
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) // enum 지원
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP) // DATE = 날짜, TIME = 시간, TIMESTAMP = 날짜+시간
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP) // 사실 근데 Date가 아니고, LocalDate, LocalDateTime을 쓰면 Temporal도 필요없음.
+    private Date lastModifiedDate;
+
+    @Lob // 제한이 없는 경우.
+    private String description;
+
+    public Member() {}
 }
--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,7 @@
 package com.example.hellojpa;
 
+import jpabook.jpashop.domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -23,28 +25,11 @@ public class JpaMain {
 
         // == Create == //
         Member member = new Member();
-        member.setId(1L);
-        member.setUsername("first");
-//        member.setName("first"); // 여기까지는 비영속
+        member.setName("hello");
+
 //
         em.persist(member); // 여기서 영속 but db에 업데이트 되지 않음.
 
-        // == read & update == //
-//        Member member = em.find(Member.class, 1L);
-//        System.out.println("member = " + member);
-//        member.setName("updated name");
-////        em.persist(); // 필요 없음
-
-        // == JPQL == //
-//        List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-//                .setFirstResult(5) // 5번부터 시작해서
-//                .setMaxResults(100) // 100 개를 가져와라.
-//                .getResultList();
-        // 주의해야 할 점 ? =>
-        // 테이블이 아니고 객체를 대상으로 쿼리를 날린다고 생각해라. (왜 중요한건지는 모르겠네..)
-
-
-        // == custom code starts == //
 
         tx.commit();
         em.close();
